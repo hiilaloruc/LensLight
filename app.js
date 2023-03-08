e24ad1a19a24025b718fa27dff8a5a8c9526e3ec
Hilal Oruç <hiilaloruc@gmail.com>
2023-03-08 23:41:00 +0300

    ejs added, partials(header,navbar,footer) files created

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//ejs template engine
+app.set("view engine", "ejs");
+
+//static files middleware(arayazılım)
+app.use(express.static("public"));
+
 //Nodejs Web application consists of a "request and response" loop...
 app.get("/", (req, res) => {
-  res.send("Hello nodeJsss! :)");
+  //res.send("Hello nodeJs! :)");
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  //res.send("Hello nodeJs! :)");
+  res.render("about");
 });
 
 app.listen(port, () => {
